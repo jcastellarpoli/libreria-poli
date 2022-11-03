7f10109510f22ee002241efabb4b54c219595841
jcastellarpoli <jcastellar@poligran.edu.co>
2022-11-02 22:15:44 -0500

    Commit inicial proyecto

---
+++ libreria-poli/src/main/java/com/libreriapoli/libreriapoli/LibreriaPoliApplication.java
@@ -0,0 +1,13 @@
+package com.libreriapoli.libreriapoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaPoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaPoliApplication.class, args);
+	}
+
+}
---
+++ libreria-poli/src/test/java/com/libreriapoli/libreriapoli/LibreriaPoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libreriapoli.libreriapoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaPoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
